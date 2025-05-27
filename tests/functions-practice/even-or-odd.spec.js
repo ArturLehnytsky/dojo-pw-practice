4086209e5790be9156c07552195925b097c6c8a2
Lekhnitskyi <arturlehnytsky@gmail.com>
2025-05-27 21:36:42 +0300

    Remove generated .js test files

--- tests/functions-practice/even-or-odd.spec.js
+++
@@ -1,9 +0,0 @@
-function evenOrOdd(num) {
-    if (num % 2 > 0) {
-        console.log("The number ".concat(num, " is odd"));
-    }
-    else {
-        console.log("The number ".concat(num, " is even"));
-    }
-}
-evenOrOdd(2);
