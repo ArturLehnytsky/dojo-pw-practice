ec20cba8876169075538a540b5487caaab3038dd
Lekhnitskyi <arturlehnytsky@gmail.com>
2025-06-02 20:43:21 +0300

    improve learningwebdriver.io test

--- tests/learnwebdriverio-com/learnwebdriver-func.spec.ts
+++ tests/learnwebdriverio-com/learnwebdriver-func.spec.ts
@@ -2,7 +2,14 @@ import { test, expect, type Page } from '@playwright/test';
 import { faker } from '@faker-js/faker';
 
 //Test-Data: User
-const user = {
+
+interface User {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const user: User = {
   name: faker.person.firstName(),
   email: faker.internet.email(),
   password: 'JHgFD&kl',
@@ -20,22 +27,22 @@ const articles = [
 const goToRegistrationForm = (page: Page) => page.locator('[href="/register"]').click();
 
 //Register new user form page
-const fillUserName = (page: Page) => page.locator('[placeholder="Username"]').fill(user.name);
-const fillUserEmail = (page: Page) => page.locator('[placeholder="Email"]').fill(user.email);
-const fillUserPassword = (page: Page) => page.locator('[placeholder="Password"]').fill(user.password);
-const clickSignUpBtn = (page: Page) => page.locator('//button[contains(text(), "Sign up")]').click();
+const getUserNameFieldLocator = (page: Page) => page.locator('[placeholder="Username"]');
+const getUserEmailFieldLocator = (page: Page) => page.locator('[placeholder="Email"]');
+const getUserPasswordFieldLocator = (page: Page) => page.locator('[placeholder="Password"]');
+const getSignUpBtnLocator = (page: Page) => page.locator('//button[contains(text(), "Sign up")]');
 
 //Create new article form page
 const goToArticleCreationPage = (page: Page) => page.locator('[href="/editor"]').click();
-const fillArticleTitle = (page: Page, title: string) => page.locator('[data-qa-id="editor-title"]').fill(title);
-const fillArticleDescription = (page: Page, desc: string) => page.locator('[data-qa-id="editor-description"]').fill(desc);
-const fillArticleText = (page: Page, text: string) => page.locator('[placeholder="Write your article (in markdown)"]').fill(text);
-const fillArticleTag = (page: Page, tag: string) => page.locator('[data-qa-id="editor-tags"]').fill(tag);
-const clickCreateArticleBtn = (page: Page) => page.locator('[data-qa-id="editor-publish"]').click();
-const createdArticleTitle = (page: Page) => page.locator('[data-qa-id="article-title"]');
+const getArticleTitleLocator = (page: Page, title: string) => page.locator('[data-qa-id="editor-title"]').fill(title);
+const getArticleDescriptionLocator = (page: Page, desc: string) => page.locator('[data-qa-id="editor-description"]').fill(desc);
+const getArticleTextLocator = (page: Page, text: string) => page.locator('[placeholder="Write your article (in markdown)"]').fill(text);
+const getArticleTagLocator = (page: Page, tag: string) => page.locator('[data-qa-id="editor-tags"]').fill(tag);
+const getCreateArticleBtnLocator = (page: Page) => page.locator('[data-qa-id="editor-publish"]').click();
+const getJustCreatedArticleTitleLocator = (page: Page) => page.locator('[data-qa-id="article-title"]');
 
 //Profile page
-const goToProfile = (page: Page) => page.locator(`//li[@class = "nav-item"]/a[@href="/@${user.name.toLowerCase()}/"]`).click();
+const goToProfile = (page: Page, user: User) => page.locator(`//li[@class = "nav-item"]/a[@href="/@${user.name.toLowerCase()}/"]`);
 const getArticlePreviewTitleLocator = (page: Page) => page.locator('//*[@data-qa-type = "preview-title"]');
 
 test.beforeEach(async ({ page }) => {
@@ -43,33 +50,33 @@ test.beforeEach(async ({ page }) => {
 });
 
 test('Should allow me to create multiple articles', { tag: '@webdriverio' }, async ({ page }) => {
-  await registerNewUser(page);
+  await goToRegistrationForm(page);
+  await fillRegistrationForm(page, user.name, user.email, user.password);
+  await getSignUpBtnLocator(page).click();
 
   for (let i = 0; i < articles.length; i++) {
     await createNewArticle(page, articles[i].title, articles[i].description, articles[i].text, articles[i].tag);
-    await expect(createdArticleTitle(page)).toHaveText(articles[i].title);
+    await expect(getJustCreatedArticleTitleLocator(page)).toHaveText(articles[i].title);
   }
 
-  await goToProfile(page);
+  await goToProfile(page, user).click();
 
   for (let i = articles.length; i <= 0; i--) {
     await expect(getArticlePreviewTitleLocator(page).nth(i)).toHaveText(articles[i].title);
   }
 });
 
-async function registerNewUser(page: Page) {
-  await goToRegistrationForm(page);
-  await fillUserName(page);
-  await fillUserEmail(page);
-  await fillUserPassword(page);
-  await clickSignUpBtn(page);
+async function fillRegistrationForm(page: Page, name?: string, email?: string, password?: string) {
+  if (name) await getUserNameFieldLocator(page).fill(name);
+  if (email) await getUserEmailFieldLocator(page).fill(email);
+  if (password) await getUserPasswordFieldLocator(page).fill(password);
 }
 
 async function createNewArticle(page: Page, title: string, desc: string, text: string, tag: string) {
   await goToArticleCreationPage(page);
-  await fillArticleTitle(page, title);
-  await fillArticleDescription(page, desc);
-  await fillArticleText(page, text);
-  await fillArticleTag(page, tag);
-  await clickCreateArticleBtn(page);
+  await getArticleTitleLocator(page, title);
+  await getArticleDescriptionLocator(page, desc);
+  await getArticleTextLocator(page, text);
+  await getArticleTagLocator(page, tag);
+  await getCreateArticleBtnLocator(page);
 }
